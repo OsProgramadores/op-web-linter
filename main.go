c670143e2b577d3a839f50ebcfc0537c9be08a27
Marco Paganini <paganini@paganini.net>
2022-11-16 19:32:28 -0800

    Added path prefix support.

--- main.go
+++ main.go
@@ -24,30 +24,31 @@ var tmpl string
 
 func main() {
 	var (
-		port      = flag.Int("port", 10000, "Specify the TCP port to listen to")
-		host      = flag.String("host", "localhost", "Hostname (used for API requests)")
-		staticdir = flag.String("staticdir", "./static", "Directory where we serve static files")
+		port       = flag.Int("port", 10000, "Specify the TCP port to listen to")
+		host       = flag.String("host", "localhost", "Hostname (used for API requests)")
+		staticdir  = flag.String("staticdir", "./static", "Directory where we serve static files")
+		pathprefix = flag.String("pathprefix", "", "Add this path to all URLs (useful for reverse proxying)")
 	)
 	flag.Parse()
 
 	// All information required to serve the form.
 	fe := &frontend{
-		LintPath:  fmt.Sprintf("http://%s:%d/lint", *host, *port),
+		LintPath:  fmt.Sprintf("http://%s:%d%s", *host, *port, *pathprefix+"/lint"),
 		Languages: getLanguagesList(),
 		StaticDir: *staticdir,
 		Template:  template.Must(template.New("form").Parse(tmpl)),
 	}
 
-	http.HandleFunc("/", fe.formHandler)                  // Serve form.
-	http.HandleFunc("/getlanguages", getLanguagesHandler) // Send list of languages back to caller.
-	http.HandleFunc("/lint", lintRequestHandler)          // Linter request.
+	http.HandleFunc(*pathprefix+"/", fe.formHandler)                  // Serve form.
+	http.HandleFunc(*pathprefix+"/getlanguages", getLanguagesHandler) // Send list of languages back to caller.
+	http.HandleFunc(*pathprefix+"/lint", lintRequestHandler)          // Linter request.
 
 	// Everything under staticURLPath is served as a regular file from rootdir.
 	// This allows us to keep local javascript files and other accessory files.
 	fs := http.FileServer(http.Dir(*staticdir))
 	http.Handle(staticURLPath, http.StripPrefix(staticURLPath, fs))
 
-	url := fmt.Sprintf("http://%s:%d", *host, *port)
+	url := fmt.Sprintf("http://%s:%d%s", *host, *port, *pathprefix)
 	log.Printf("Listening on %s", url)
 	log.Printf("Serving static files on %s", url+staticURLPath)
 
